7b3733333370bf275bb6b8fb08a19398cc5188e8
AjeetVIT <ajeetvit@gmail.com>
2022-02-02 16:55:34 +0530

    spec repo update

---
+++ src/main/java/in/nareshit/ajeet/hc/repo/SpecializationRepository.java
@@ -0,0 +1,9 @@
+package in.nareshit.ajeet.hc.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.nareshit.ajeet.hc.entity.Specialization;
+
+public interface SpecializationRepository extends JpaRepository<Specialization, Long> {
+
+}
